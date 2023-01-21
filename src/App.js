3131ce1eb1f1efd453cc44c72f47199b2200cd05
salgueiro23 <luis.salgueiro@yduqs.com.br>
2023-01-21 19:37:10 -0300

    atualizacao

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from 'react';
+
+function Registros({ list = [] }){
+
+
+  const [ order, setOrder ] = useState(1)
+  const [ colunmOrder, setcolunmOrder ] = useState('level')
+
+  const handleOrder = fieldName => {
+    setOrder(-order)
+    setcolunmOrder(fieldName)
+  }
+
+ list = list.sort((a,b) => {
+  return a[colunmOrder] < b[colunmOrder] ? -order : order;
+ })
+
+
+
+
+
+
+  return <table>
+
+    <thead>
+      <tr>
+      <th onClick={ e => handleOrder('foto')}></th>
+        <th onClick={ e => handleOrder('player')}>Jogador</th>
+        <th onClick={ e => handleOrder('team')}>Time</th>
+        <th onClick={ e => handleOrder('level')}>Level</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {
+        list.map(({foto, player, level, team}) => {
+          return <tr key={player}>
+              <td><img src={foto} alt="Jogador"/></td>
+              <td>{player}</td>
+              <td>{team}</td>
+              <td>{level}</td>
+              
+          </tr>
+        })
+      }
+
+    </tbody>
+
+  </table>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [ list, setList ] = useState ( [] )
+
+  useEffect ( () => {
+
+    fetch ('http://localhost:3000/produtos').then ( async result => {
+      setList ( await result.json() )
+    })
+
+  }, [] )
+ 
+
+
+
+  return <div>
+    <Registros list={list} />
+  </div>
+    
 }
 
 export default App;
